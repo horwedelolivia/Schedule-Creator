77ad41974bf511060d15b02c2cbe823afb8158f1
Olivia Horwedel <ohorwede@asu.edu>
2024-02-27 15:53:05 -0500

    Added in new styling and added side bar

---
+++ lib/styles/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+    static const TextStyle appBarText = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle headingText = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.normal,
+    color: Colors.black,
+  );
+}
